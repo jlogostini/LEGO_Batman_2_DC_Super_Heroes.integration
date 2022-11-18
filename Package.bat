853da95b8700bafc946f807da3155a2b58e3063a
John Logostini <johnlogostini16485@gmail.com>
2022-11-18 07:35:12 -0400

    .bat test 013

--- Package.bat
+++ Package.bat
@@ -1,5 +1,5 @@
 mkdir .temp-253476
-xcopy assets /t /e .temp-253476
-xcopy tools /t /e .temp-253476
+xcopy assets .temp-253476 /t /e
+xcopy tools .temp-253476 /t /e
 copy config.json .temp-253476
 cd .temp-253476
