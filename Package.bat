5574a637ac3684557aaf37029936f33bf4252caf
John Logostini <johnlogostini16485@gmail.com>
2022-11-18 07:27:36 -0400

    .bat test 008

--- Package.bat
+++ Package.bat
@@ -1,6 +1,5 @@
 mkdir .temp-253476
-xcopy assets /E /H /C /I .temp-253476
-xcopy tools /E /H /C /I .temp-253476
+xcopy assets /E /H /C /I .temp-253476/assets
+xcopy tools /E /H /C /I .temp-253476/tools
 copy config.json .temp-253476
 cd .temp-253476
-zip -r LEGO_Batman_2_DC_Super_Heroes.integration *\
