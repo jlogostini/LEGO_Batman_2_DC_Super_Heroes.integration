9f0f1798093faaecb2f4283766149ca11fe41ca8
John Logostini <johnlogostini16485@gmail.com>
2022-11-18 07:26:27 -0400

    .bat test 008

--- Package.bat
+++ Package.bat
@@ -3,4 +3,4 @@ xcopy assets /E /H /C /I .temp-253476
 xcopy tools /E /H /C /I .temp-253476
 copy config.json .temp-253476
 cd .temp-253476
-zip -r LEGO_Batman_2_DC_Super_Heroes.integration assets\ tools\ config.json
+zip -r LEGO_Batman_2_DC_Super_Heroes.integration *\
