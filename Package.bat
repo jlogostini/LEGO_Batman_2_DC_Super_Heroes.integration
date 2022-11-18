e2fdc4b88144225a9c54e718de444254c3a78d91
John Logostini <johnlogostini16485@gmail.com>
2022-11-18 08:02:16 -0400

    .bat test 025

--- Package.bat
+++ Package.bat
@@ -1,8 +1,8 @@
 mkdir .temp-253476
-xcopy assets .temp-253476/LEGO_Batman_2_DC_Super_Heroes.integration /S /Y /R
-xcopy tools .temp-253476/LEGO_Batman_2_DC_Super_Heroes.integration /S /Y /R
-copy config.json .temp-253476/LEGO_Batman_2_DC_Super_Heroes.integration
-cd .temp-253476/LEGO_Batman_2_DC_Super_Heroes.integration
+xcopy assets .temp-253476 /S /Y /R
+xcopy tools .temp-253476 /S /Y /R
+copy config.json .temp-253476
+cd .temp-253476/
 mkdir assets
 mkdir tools
 move art assets
@@ -10,4 +10,4 @@ move local-art assets
 move mod-types assets
 move scripts tools
 move programs tools
-powershell Compress-Archive . publish.zip
+powershell Compress-Archive . LEGO_Batman_2_DC_Super_Heroes.integration
